cc99ab550e7429a31c3591f2eadc2c9ce6f1125d
s299108 <malcolm.cantin@ocsbstudent.ca>
2020-02-28 13:43:00 -0500

    Initial commit

---
+++ DrawingShapesMalcolm/main.lua
@@ -0,0 +1,16 @@
+-----------------------------------------------------------------------------------------
+--
+-- main.lua
+--
+-----------------------------------------------------------------------------------------
+
+-- Your code here
+
+-- create my local variables
+
+
+-- set the background colour of my screen
+display.setDefault("background", /255, /255, /255)
+
+-- to remove status bar
+display.setStatusBar(display.HiddenStatus)
