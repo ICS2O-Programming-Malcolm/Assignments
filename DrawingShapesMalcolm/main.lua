c5d5127e21be876d6c084b26476330ebbd774d8a
s299108 <malcolm.cantin@ocsbstudent.ca>
2020-03-03 17:27:42 -0500

    Assignment #3 Complete

--- DrawingShapesMalcolm/main.lua
+++ DrawingShapesMalcolm/main.lua
@@ -2,7 +2,9 @@
 -- Title: DrawingShapesMalcolm
 -- Name: Malcolm Cantin
 -- Course: ICS2O
--- This program displays four shapes 
+-- This program displays four different shapes; a pentagon with a gradient fill, a 
+-- triangle with it's area calculated and displayed, an octagon, and a hexagon with
+-- an image inside of it.
 -----------------------------------------------------------------------------------------
 
 -- Your code here
@@ -11,27 +13,44 @@
 local myPentagon 
 local myPentagonVertices = {50, -50, -80, -40, -70, 60, -20, 20, 80, 60}
 local myPentagonText 
+local myPentagonPaint = {
+    type = "gradient",
+    color1 = {0, 0, 0, 1},
+    color2 = {1, 0, 1, 0.2},
+    direction = "down"
+}
+
 local myTriangle 
 local myTriangleVertices = {-50, 60, 70, 30, -10, -40}
 local myTriangleText
+local myTriangleBase = 100
+local myTriangleHeight = 75
+local myTriangleArea
+local myTriangleAreaText
+
 local myOctagon
 local myOctagonVertices = {-20, 60, 20, 60, 60, 20, 60, -20, 20, -60, -20, -60, -60, -20, -60, 20}
 local myOctagonText
+
 local myHexagon 
 local myHexagonVertices = {-80, 50, 0, 50, 60, 0, -10, -70, -30, 10, -60, -40}
 local myHexagonText
+local myHexagonPaint = {
+    type = "image",
+    filename = "Images/coding.jpg" 
+}
 
 -- set the background colour of my screen
 display.setDefault("background", 155/255, 11/255, 11/255)
 
--- to remove status bar
+-- removing status bar
 display.setStatusBar(display.HiddenStatusBar)
 
 -- PENTAGON
 -- draw the pentagon that is in the top left of the screen
 myPentagon = display.newPolygon(display.contentWidth/4, display.contentHeight/4, myPentagonVertices)
 
--- anchor the pentagon at the top left corner of the screen and set its (x,y) position
+-- set the (x,y) position of the pentagon 
 myPentagon.x = display.contentWidth/4
 myPentagon.y = display.contentHeight/4
 
@@ -47,14 +66,18 @@ myPentagon:setStrokeColor(0, 1, 1)
 -- flip the pentagon
 myPentagon:scale(1, -1)
 
--- label the pentagon
-myPentagonText = display.newText("Pentagon", myPentagon.x, myPentagon.y - 100, Arial, 50) 
+-- label the pentagon and colour the text
+myPentagonText = display.newText("Pentagon", myPentagon.x, myPentagon.y + 100, Arial, 50)
+myPentagonText:setTextColor(255, 0, 0) 
+
+-- add gradient fill to the inside of the pentagon
+myPentagon.fill = myPentagonPaint
 
 -- TRIANGLE
 -- draw the triangle that is in the top right of the screen
 myTriangle = display.newPolygon(display.contentWidth*(3/4), display.contentHeight/4, myTriangleVertices)
 
--- anchor the triangle at the top right corner of the screen
+-- set the (x,y) position of the triangle
 myTriangle.x = display.contentWidth*(3/4)
 myTriangle.y = display.contentHeight/4
 
@@ -64,20 +87,27 @@ myTriangle.strokeWidth = 15
 -- set the colour of the triangle
 myTriangle:setFillColor(0.3, 0.7, 0.6)
 
--- set the colour of the border if the triangle
+-- set the colour of the border of the triangle
 myTriangle:setStrokeColor(1, 0.5, 0)
 
 -- flip the triangle
 myTriangle:scale(1, -1)
 
--- label the triangle
-myTriangleText = display.newText("Triangle", myTriangle.x, myTriangle.y - 100, Arial, 50) 
+-- label the triangle and colour the text
+myTriangleText = display.newText("Triangle", myTriangle.x, myTriangle.y + 100, Arial, 50) 
+myTriangleText:setTextColor(21, 0, 43) 
+
+-- calculate and display the area of the triangle
+myTriangleArea = myTriangleBase * myTriangleHeight / 2
+myTriangleAreaText = display.newText("The area of this triangle with a base of \n" .. 
+    myTriangleBase .. " and a height of " .. myTriangleHeight .. " is " .. 
+    myTriangleArea .. " pixels².", myTriangle.x, myTriangle.y + 200, Arial, 25)
 
 -- OCTAGON
 -- draw the octagon that is in the bottom left of the screen
 myOctagon = display.newPolygon(display.contentWidth/4, display.contentHeight*(3/4), myOctagonVertices)
 
--- anchor the octagon at the bottom left corner of the screen
+-- set the (x,y) position of the octagon
 myOctagon.x = display.contentWidth/4
 myOctagon.y = display.contentHeight*(3/4)
 
@@ -87,20 +117,21 @@ myOctagon.strokeWidth = 15
 -- set the colour of the octagon
 myOctagon:setFillColor(0.7, 0.7, 0.7)
 
--- set the colour of the border if the octagon
+-- set the colour of the border of the octagon
 myOctagon:setStrokeColor(0, 0, 1)
 
 -- flip the octagon
 myOctagon:scale(1, -1)
 
--- label the octagon
-myOctagonText = display.newText("Octagon", myOctagon.x, myOctagon.y - 100, Arial, 50) 
+-- label the octagon and colour the text
+myOctagonText = display.newText("Octagon", myOctagon.x, myOctagon.y + 100, Arial, 50) 
+myOctagonText:setTextColor(0, 0, 0) 
 
 -- HEXAGON
 -- draw the hexagon that is in the bottom right of the screen
 myHexagon = display.newPolygon(display.contentWidth*(3/4), display.contentHeight*(3/4), myHexagonVertices)
 
--- anchor the hexagon at the top bottom right of the screen and set its (x,y) position
+-- set the (x,y) position of the hexagon
 myHexagon.x = display.contentWidth*(3/4)
 myHexagon.y = display.contentHeight*(3/4)
 
@@ -110,11 +141,15 @@ myHexagon.strokeWidth = 15
 -- set the colour of the hexagon
 myHexagon:setFillColor(0.2, 0.2, 0.5)
 
--- set the colour of the border
+-- set the colour of the border of the hexagon
 myHexagon:setStrokeColor(0, 1, 0)
 
 -- flip the hexagon
 myHexagon:scale(1, -1)
 
--- label the hexagon
-myHexagonText = display.newText("Hexagon", myHexagon.x, myHexagon.y - 100, Arial, 50) 
+-- label the hexagon and colour the text
+myHexagonText = display.newText("Hexagon", myHexagon.x, myHexagon.y + 100, Arial, 50) 
+myHexagonText:setTextColor(255, 255, 0) 
+
+-- insert an image into the hexagon
+myHexagon.fill = myHexagonPaint
