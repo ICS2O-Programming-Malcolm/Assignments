2213d803858f9bcd45ec4c20b0ac76d5c64f5fd3
s299108 <malcolm.cantin@ocsbstudent.ca>
2020-03-08 15:56:14 -0400

    Update main.lua

--- AnimatingImagesMalcolm/main.lua
+++ AnimatingImagesMalcolm/main.lua
@@ -6,35 +6,50 @@
 -----------------------------------------------------------------------------------------
 -- Your code here
 
--- CODE FOR THE BLACKHOLE
 -- hide the status bar
 display.setStatusBar(display.HiddenStatusBar)
 
 -- global variables
 scrollSpeed = 3
 
+-- create text variable
+local programText
+
 -- background image with width and height
 local backgroundImage = display.newImageRect("Images/background.jpg", 2048, 1536)
 
--- character image with width and height
-local blackhole = display.newImageRect("Images/blackhole.png", 400, 200)
+-- label the program and colour the text
+programText = display.newText("Super Silly Space", display.contentWidth/2 + 120, display.contentHeight/1 - 80, Arial, 80)
+programText:setTextColor(255/255, 106/255, 0/255) 
 
--- set the image to be transparent
-blackhole.alpha = 0
+-- CODE FOR THE BLACKHOLE
+-- character image with width and height
+local blackholeWidth = 400
+local blackholeHeight = 200
+local blackhole = display.newImageRect("Images/blackhole.png", blackholeWidth, blackholeHeight)
+
+-- set the image to be transparent at first
+blackhole.alpha = 1
 
 -- set the initial x and y position of blackhole
 blackhole.x = 0
-blackhole.y = display.contentHeight/3
+blackhole.y = display.contentHeight/4
+
+-- global blackhole variables
+x = blackhole.x
+y = blackhole.y
 
 -- Function: MoveBlackhole
 -- Input: this function accepts an event listener
 -- Output: none
 -- Description: This function adds the scroll speed to the x-value of the blackhole
 local function MoveBlackhole(event)
+	-- movement path
+	y = x
 	-- add the scroll speed to the x-value of the blackhole
 	blackhole.x = blackhole.x + scrollSpeed
-	-- change the transparency of the blackhole every time it moves so that it fades out
-	blackhole.alpha = blackhole.alpha + 0.01
+	-- make the blackhole grow as it moves
+	blackhole:scale(1, 1)
 end
 
 -- MoveBlackhole will be called over and over again
@@ -44,54 +59,54 @@ Runtime:addEventListener("enterFrame", MoveBlackhole)
 -- character image with width and height
 local rocketship = display.newImageRect("Images/rocketship.png", 200, 200)
 
--- set the image to be transparent
+-- set the image to be visible at first
 rocketship.alpha = 1
 
 -- set the initial x and y position of rocketship
 rocketship.x = 1024
 rocketship.y = display.contentHeight*(2/3)
 
--- Function: MoveShip
+-- Function: MoveRocketship
 -- Input: this function accepts an event listener
 -- Output: none
 -- Description: This function adds the scroll speed to the x-value of the ship
-local function MoveRocket(event)
+local function MoveRocketship(event)
 	-- add the scroll speed to the x-value of the ship
 	rocketship.x = rocketship.x - scrollSpeed - 2
-	-- change the transparency of the ship every time it moves so that it fades out
-	rocketship.alpha = rocketship.alpha - 0.000000001
+	-- if ship reaches border of screen, it will turn around
+	if rocketship.x = 0 then
+	rocketship.x = rocketship.x + scrollSpeed + 2
+
 end
 
--- MoveRocket will be called over and over again
-Runtime:addEventListener("enterFrame", MoveRocket)
+-- MoveRocketship will be called over and over again
+Runtime:addEventListener("enterFrame", MoveRocketship)
 
 -- flip the rocket
 rocketship:scale(-1, 1)
 
--- CODE FOR THE ROCKETSHIP
+-- CODE FOR THE ALIEN
 -- character image with width and height
-local rocketship = display.newImageRect("Images/rocketship.png", 200, 200)
+local alien = display.newImageRect("Images/alien.png", 200, 200)
 
--- set the image to be transparent
-rocketship.alpha = 1
+-- set the image to be transparent at first
+alien.alpha = 1
 
--- set the initial x and y position of rocketship
-rocketship.x = 1024
-rocketship.y = display.contentHeight*(2/3)
+-- set the initial x and y position of alien
+alien.x = 0
+alien.y = 768
 
--- Function: MoveShip
+-- Function: MoveAlien
 -- Input: this function accepts an event listener
 -- Output: none
--- Description: This function adds the scroll speed to the x-value of the ship
-local function MoveRocket(event)
-	-- add the scroll speed to the x-value of the ship
-	rocketship.x = rocketship.x - scrollSpeed - 2
-	-- change the transparency of the ship every time it moves so that it fades out
-	rocketship.alpha = rocketship.alpha - 0.000000001
+-- Description: This function adds the scroll speed to the x-value of the alien
+local function MoveAlien(event)
+	-- add the scroll speed to the x-value of the alien
+	alien.x = alien.x + scrollSpeed + 2
+	alien.y = alien.y - scrollSpeed - 2
+	-- change the transparency of the alien every time it moves so that it fades out
+	alien.alpha = alien.alpha - 0.000000001
 end
 
--- MoveRocket will be called over and over again
-Runtime:addEventListener("enterFrame", MoveRocket)
-
--- flip the rocket
-rocketship:scale(-1, 1)
+-- MoveAlien will be called over and over again
+Runtime:addEventListener("enterFrame", MoveAlien)
